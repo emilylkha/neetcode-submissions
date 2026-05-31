19084737971314edc0722c43cea8010df07ea367
emily <111032070+emilylkha@users.noreply.github.com>
2026-05-31 20:49:23 +1000

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.py
@@ -0,0 +1,11 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # put it into some representation to easily check
+        anagrams = {} # k = representation, v = word
+        for word in strs:
+            rep = [0] * 26
+            for letter in word:
+                rep[ord(letter) - ord('a')] += 1
+            anagrams[str(rep)] = anagrams.get(str(rep), [])
+            anagrams[str(rep)].append(word)
+        return list(anagrams.values())
